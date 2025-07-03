66b0fa848644a30ddfe0fcb6dd1d0bc1ae6ae8ed
Manoj-N00 <manojnaikj@gmail.com>
2025-07-03 17:30:36 +0530

    fix

--- apps/excelidraw-frontend/next.config.js
+++ apps/excelidraw-frontend/next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
     eslint: {
       ignoreDuringBuilds: true,
     },
     typescript: {
       ignoreBuildErrors: true,
     },
-    experimental: {
-      swcMinify: true
-    }
   };
   
   export default nextConfig;
