45a99fa1d82791b6805d63daca5fc7de40f6f9a3
Manoj-N00 <manojnaikj@gmail.com>
2025-08-15 16:13:53 +0530

    Fix:Next config

--- apps/excelidraw-frontend/next.config.js
+++ apps/excelidraw-frontend/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: '.next',
   webpack: (config, { isServer }) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
